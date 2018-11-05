d2a76984c7eda54ace669a76569d06d6f50f1327
duhongming1990 <13342496610@189.cn>
2018-11-05 19:58:14 +0800

    循环链表： 魔术师发牌问题 拉丁方阵问题
    双向链表
    
    栈：LIFO

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        int i = Integer.valueOf("11001001",2);
+        String s = Integer.toOctalString(i);
+        String ss = Integer.toHexString(i);
         SpringApplication.run(DemoApplication.class, args);
     }
 }

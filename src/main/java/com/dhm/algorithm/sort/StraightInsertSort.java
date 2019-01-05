1e15f9c324881d5471f06dd9225f6ea041440167
duhongming1990 <13342496610@189.cn>
2019-01-05 19:53:55 +0800

    直接插入排序

---
+++ src/main/java/com/dhm/algorithm/sort/StraightInsertSort.java
@@ -0,0 +1,35 @@
+package com.dhm.algorithm.sort;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author duhongming
+ * @Email 935720334@qq.com
+ * @Date 2019/1/5 19:14
+ */
+@Slf4j
+public class StraightInsertSort {
+
+    public static void main(String[] args) {
+        int[] data = {5, 2, 6, 0, 3, 9, 1, 7, 4, 8};
+        sort(data);
+    }
+
+    public static void sort(int[] data) {
+        System.out.println("排序前 = " + JSON.toJSONString(data));
+        int count1 = 0;
+        for (int i = 1; i < data.length; i++) {
+            int temp = data[i];
+            int j = i - 1;
+            while (j >= 0 && temp < data[j]) {
+                data[j + 1] = data[j];
+                count1++;
+                j--;
+            }
+            data[j + 1] = temp;
+        }
+        log.debug("总共进行了{}次移动！", count1);
+        System.out.println("排序后 = " + JSON.toJSONString(data));
+    }
+}
--- src/main/java/com/dhm/vm/gc/TestPretenureSizeThreshold.java
+++ src/main/java/com/dhm/vm/gc/TestPretenureSizeThreshold.java
@@ -7,13 +7,13 @@ package com.dhm.vm.gc;
  * @Date 2019/1/3 14:54
  *
  * 对象超过3M 时直接进入老年代
- * -verbose:gc -Xms20m -Xmx20m -Xmn10m -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:PretenureSizeThreshold=3145728
+ * -verbose:gc -Xms20m -Xmx20m -Xmn5m -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:PretenureSizeThreshold=3145728
  */
 public class TestPretenureSizeThreshold {
     public static final int _1M = 1 * 1024 * 1024;
 
     public static void main(String[] args) {
         byte[] allocation1;
-        allocation1 = new byte[4 * _1M];//直接分配在老年代中
+        allocation1 = new byte[10 * _1M];//直接分配在老年代中
     }
 }
